9a17b1d853ac296ac28cab51ffd6336386c7934b
Kim jisu <jisu0259@naver.com>
2025-12-14 15:36:07 +0900

    build: 프로젝트 세팅 #1 - config 설정

---
+++ src/main/java/me/singingsandhill/calendar/infrastructure/config/WebConfig.java
@@ -0,0 +1,8 @@
+package me.singingsandhill.calendar.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+}
